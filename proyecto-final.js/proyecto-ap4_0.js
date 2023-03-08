93d19a8ad2e4c4f38a9ad55de2f079f35e7af495
Fermin Mamani <alejandroferminm@gmail.com>
2023-03-08 16:47:58 -0300

    repositorio de proyecto final

---
+++ proyecto-final.js/proyecto-ap4_0.js
@@ -0,0 +1,9 @@
+
+document.getElementById('boton_claro').addEventListener('click', function() {
+    document.body.style.background = '#afaa8f';
+});
+
+
+document.getElementById('boton_oscuro').addEventListener('click', function() {
+    document.body.style.background = '#3d3838';
+});
